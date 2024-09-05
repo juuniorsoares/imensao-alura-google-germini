ac4a367a093e827cca5b7dc121d15f300560608f
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-09-04 21:13:49 -0300

    adicionados os dados dos filmes
    Foi feito uma lista de objetos com todas informações dos filmes que serão pesquisados.

---
+++ dados.js
@@ -0,0 +1,32 @@
+let listaDeFilmes = [
+    {
+        titulo: "O Poderoso Chefão",
+        genero: "Crime, Drama",
+        sinopse: "A saga de uma poderosa família mafiosa italiana nos Estados Unidos.",
+        ano: 1972
+    },
+    {
+        titulo: "A Vida é Bela",
+        genero: "Comédia Dramática, Guerra",
+        sinopse: "Um pai judeu usa a imaginação para proteger seu filho em um campo de concentração nazista.",
+        ano: 1997
+    },
+    {
+        titulo: "Mad Max: Estrada da Fúria",
+        genero: "Ação, Ficção Científica",
+        sinopse: "Em um futuro pós-apocalíptico, uma guerreira lidera uma revolta contra um tirano.",
+        ano: 2015
+    },
+    {
+        titulo: "Parasita",
+        genero: "Thriller, Drama",
+        sinopse: "Duas famílias de classes sociais opostas se cruzam de forma trágica.",
+        ano: 2019
+    },
+    {
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+        genero: "Fantasia, Aventura",
+        sinopse: "Um hobbit se junta a um grupo de companheiros em uma jornada para destruir um anel poderoso.",
+        ano: 2001
+    }
+];
