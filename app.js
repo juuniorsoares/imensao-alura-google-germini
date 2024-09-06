658f40cf251d7368c3d930f895b502b9bbaad5e9
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-09-05 23:20:15 -0300

    deixando a aplicação mais interativa

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log(listaDeFilmes);
+// Função para pesquisar filmes e exibir os resultados na página
+function pesquisar() {
+    console.log("clicou"); // Indica que a função foi chamada
+};
+
+// Obter a seção onde os resultados serão exibidos
+let section = document.getElementById("resultados-pesquisa");
+
+// Variável para armazenar o HTML dos resultados
+let resultados = "";
+
+// Iterar sobre os dados dos filmes e criar o HTML para cada filme
+for (let dado of dados) {
+    // Criar um elemento div para cada resultado
+    resultados += `
+    <div class="item-resultado">
+        <h2>
+            <a href="https://pt.wikipedia.org/wiki/Surf%27s_Up" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">Sinopse: ${dado.sinopse}.</p>
+        <p class="descricao-meta">Gênero: ${dado.genero}.</p>
+        <p class="descricao-meta"> Ano de lançamento: ${dado.ano}.</p>
+        <a href=${dado.link} target="_blank">Mais Informações</a>
+    </div>
+    `
+}
+
+// Atualizar o conteúdo da seção com os resultados da pesquisa
+section.innerHTML = resultados;
+
+
+
+
+
--- dados.js
+++ dados.js
@@ -1,32 +1,37 @@
-let listaDeFilmes = [
+let dados = [
     {
         titulo: "O Poderoso Chefão",
         genero: "Crime, Drama",
-        sinopse: "A saga de uma poderosa família mafiosa italiana nos Estados Unidos.",
-        ano: 1972
+        sinopse: "A saga de uma poderosa família mafiosa italiana nos Estados Unidos",
+        ano: 1972,
+        link: "https://pt.wikipedia.org/wiki/The_Godfather"
     },
     {
         titulo: "A Vida é Bela",
         genero: "Comédia Dramática, Guerra",
-        sinopse: "Um pai judeu usa a imaginação para proteger seu filho em um campo de concentração nazista.",
-        ano: 1997
+        sinopse: "Um pai judeu usa a imaginação para proteger seu filho em um campo de concentração nazista",
+        ano: 1997,
+        link: "https://pt.wikipedia.org/wiki/A_Vida_%C3%89_Bela"
     },
     {
         titulo: "Mad Max: Estrada da Fúria",
         genero: "Ação, Ficção Científica",
-        sinopse: "Em um futuro pós-apocalíptico, uma guerreira lidera uma revolta contra um tirano.",
-        ano: 2015
+        sinopse: "Em um futuro pós-apocalíptico, uma guerreira lidera uma revolta contra um tirano",
+        ano: 2015,
+        link: "https://pt.wikipedia.org/wiki/Mad_Max:_Estrada_da_F%C3%BAria"
     },
     {
         titulo: "Parasita",
         genero: "Thriller, Drama",
-        sinopse: "Duas famílias de classes sociais opostas se cruzam de forma trágica.",
-        ano: 2019
+        sinopse: "Duas famílias de classes sociais opostas se cruzam de forma trágica",
+        ano: 2019,
+        link: "https://pt.wikipedia.org/wiki/Gisaengchung"
     },
     {
         titulo: "O Senhor dos Anéis: A Sociedade do Anel",
         genero: "Fantasia, Aventura",
-        sinopse: "Um hobbit se junta a um grupo de companheiros em uma jornada para destruir um anel poderoso.",
-        ano: 2001
+        sinopse: "Um hobbit se junta a um grupo de companheiros em uma jornada para destruir um anel poderoso",
+        ano: 2001,
+        link: "https://pt.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Fellowship_of_the_Ring"
     }
 ];
